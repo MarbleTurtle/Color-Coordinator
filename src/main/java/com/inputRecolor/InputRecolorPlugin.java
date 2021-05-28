fcae550311df23c103bc45c9c7118a884a349bf7
MarbleTurtle <60723971+MarbleTurtle@users.noreply.github.com>
2021-05-28 15:37:15 -0700

    Colors using chats colors Works with targeted mode

--- src/main/java/com/inputRecolor/InputRecolorPlugin.java
+++ src/main/java/com/inputRecolor/InputRecolorPlugin.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.VarClientStr;
+import net.runelite.api.Varbits;
 import net.runelite.api.events.ScriptPostFired;
 import net.runelite.api.events.ScriptPreFired;
 import net.runelite.api.widgets.Widget;
@@ -14,6 +15,8 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+
+import java.awt.*;
 
 @Slf4j
 @PluginDescriptor(
@@ -25,9 +28,13 @@ public class InputRecolorPlugin extends Plugin
 	private Client client;
 
 	@Inject
-	private InputRecolorConfig config;
+	private ConfigManager configManager;
 
 	int selected=0;
+	Color friendColorConfigColor;
+	Color clanColorConfigColor;
+	Color guestColorConfigColor;
+	Color unselectedColorConfigColor;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,33 +53,192 @@ public class InputRecolorPlugin extends Plugin
 	{
 		if(scriptPostFired.getScriptId()!=73&&scriptPostFired.getScriptId()!=175)
 			return;
+		boolean transparent = client.isResized() && client.getVar(Varbits.TRANSPARENT_CHATBOX) != 0;
 		Widget chat = client.getWidget(WidgetInfo.CHATBOX_INPUT);
 		if (chat == null)
 			return;
-		String name = client.getLocalPlayer().getName() + ":";
+		String name = chat.getText().contains(":") ? chat.getText().split(":")[0] + ":" : client.getLocalPlayer().getName() + ":";
 		String text = client.getVar(VarClientStr.CHATBOX_TYPED_TEXT);
-		if (text.startsWith("///") || text.startsWith("/g ")) {
-			text = " <col="+Integer.toHexString(config.guestColor().getRGB()).substring(2)+">" + text + "*</col>";
-			chat.setText(name + text);
-		} else if (text.startsWith("//") || text.startsWith("/c ")) {
-			text = " <col="+Integer.toHexString(config.clanColor().getRGB()).substring(2)+">" + text + "*</col>";
-			chat.setText(name + text);
-		} else if (text.startsWith("/")) {
-			text = " <col="+Integer.toHexString(config.friendColor().getRGB()).substring(2)+">" + text + "*</col>";
-			chat.setText(name + text);
-		} else {
-			if(selected==0){
-				text = " <col="+Integer.toHexString(config.publicColor().getRGB()).substring(2)+">" + text + "*</col>";
+		switch (text){
+			case "/p":
+				unselectedColorConfigColor = transparent ? new Color(0x9090ff) : new Color(0x0000FF);
+				text = " <col=" + Integer.toHexString(unselectedColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
 				chat.setText(name + text);
-			}else if(selected==1){
-				text = " <col="+Integer.toHexString(config.clanColor().getRGB()).substring(2)+">" + text + "*</col>";
+				break;
+			case "/f":
+				friendColorConfigColor = configManager.getConfiguration("textrecolor", transparent ? "transparentClanChatMessage" : "opaqueClanChatMessage", Color.class);
+				if (friendColorConfigColor == null) {
+					int colorCode = transparent ? client.getVarpValue(3004) - 1 : client.getVarpValue(2996) - 1;
+					if (colorCode == 0) {
+						friendColorConfigColor = Color.BLACK;
+					} else if (colorCode == -1) {
+						friendColorConfigColor = transparent ? new Color(0xEF5050) : new Color(8323072);
+					} else {
+						friendColorConfigColor = new Color(colorCode);
+					}
+				}
+				text = " <col=" + Integer.toHexString(friendColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
 				chat.setText(name + text);
-			}else if(selected==2){
-				text = " <col="+Integer.toHexString(config.friendColor().getRGB()).substring(2)+">" + text + "*</col>";
+				break;
+			case "/c":
+				clanColorConfigColor = configManager.getConfiguration("textrecolor", transparent ? "transparentClanMessage" : "opaqueClanMessage", Color.class);
+				if (clanColorConfigColor == null) {
+					int colorCode = transparent ? client.getVarpValue(3005) - 1 : client.getVarpValue(2997) - 1;
+					if (colorCode == 0) {
+						clanColorConfigColor = Color.BLACK;
+					} else if (colorCode == -1) {
+						clanColorConfigColor = transparent ? new Color(8323072) : new Color(8323072);
+					} else {
+						clanColorConfigColor = new Color(colorCode);
+					}
+				}
+				text = " <col=" + Integer.toHexString(clanColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
 				chat.setText(name + text);
+				break;
+			case "/g":
+				guestColorConfigColor = configManager.getConfiguration("textrecolor", transparent ? "transparentClanGuestMessage" : "opaqueClanGuestMessage", Color.class);
+				if (guestColorConfigColor == null) {
+					int colorCode = transparent ? client.getVarpValue(3005) - 1 : client.getVarpValue(2997) - 1;
+					if (colorCode == 0) {
+						guestColorConfigColor = Color.BLACK;
+					} else if (colorCode == -1) {
+						guestColorConfigColor = transparent ? new Color(8323072) : new Color(8323072);
+					} else {
+						guestColorConfigColor = new Color(colorCode);
+					}
+				}
+				text = " <col=" + Integer.toHexString(guestColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
+				chat.setText(name + text);
+				break;
+			default:
+			switch (name) {
+				case "Friends Chat:":
+					if(client.getFriendsChatManager()==null)
+						return;
+					friendColorConfigColor = configManager.getConfiguration("textrecolor", transparent ? "transparentClanChatMessage" : "opaqueClanChatMessage", Color.class);
+					if (friendColorConfigColor == null) {
+						int colorCode = transparent ? client.getVarpValue(3004) - 1 : client.getVarpValue(2996) - 1;
+						if (colorCode == 0) {
+							friendColorConfigColor = Color.BLACK;
+						} else if (colorCode == -1) {
+							friendColorConfigColor = transparent ? new Color(0xEF5050) : new Color(8323072);
+						} else {
+							friendColorConfigColor = new Color(colorCode);
+						}
+					}
+					text = " <col=" + Integer.toHexString(friendColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
+					chat.setText(name + text);
+					break;
+				case "Clan Chat:":
+					clanColorConfigColor = configManager.getConfiguration("textrecolor", transparent ? "transparentClanMessage" : "opaqueClanMessage", Color.class);
+					if (clanColorConfigColor == null) {
+						int colorCode = transparent ? client.getVarpValue(3005) - 1 : client.getVarpValue(2997) - 1;
+						if (colorCode == 0) {
+							clanColorConfigColor = Color.BLACK;
+						} else if (colorCode == -1) {
+							clanColorConfigColor = transparent ? new Color(8323072) : new Color(8323072);
+						} else {
+							clanColorConfigColor = new Color(colorCode);
+						}
+					}
+					text = " <col=" + Integer.toHexString(clanColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
+					chat.setText(name + text);
+					break;
+				case "Guest Clan Chat:":
+					guestColorConfigColor = configManager.getConfiguration("textrecolor", transparent ? "transparentClanGuestMessage" : "opaqueClanGuestMessage", Color.class);
+					if (guestColorConfigColor == null) {
+						int colorCode = transparent ? client.getVarpValue(3005) - 1 : client.getVarpValue(2997) - 1;
+						if (colorCode == 0) {
+							guestColorConfigColor = Color.BLACK;
+						} else if (colorCode == -1) {
+							guestColorConfigColor = transparent ? new Color(8323072) : new Color(8323072);
+						} else {
+							guestColorConfigColor = new Color(colorCode);
+						}
+					}
+					text = " <col=" + Integer.toHexString(guestColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
+					chat.setText(name + text);
+					break;
+				default:
+					if (text.startsWith("///") || (text.startsWith("/g ") || text.matches("/g"))) {
+						guestColorConfigColor = configManager.getConfiguration("textrecolor", transparent ? "transparentClanGuestMessage" : "opaqueClanGuestMessage", Color.class);
+						if (guestColorConfigColor == null) {
+							int colorCode = transparent ? client.getVarpValue(3005) - 1 : client.getVarpValue(2997) - 1;
+							if (colorCode == 0) {
+								guestColorConfigColor = Color.BLACK;
+							} else if (colorCode == -1) {
+								guestColorConfigColor = transparent ? new Color(8323072) : new Color(8323072);
+							} else {
+								guestColorConfigColor = new Color(colorCode);
+							}
+						}
+						text = " <col=" + Integer.toHexString(guestColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
+						chat.setText(name + text);
+					} else if (text.startsWith("//") || (text.startsWith("/c ") || text.matches("/c"))) {
+						clanColorConfigColor = configManager.getConfiguration("textrecolor", transparent ? "transparentClanMessage" : "opaqueClanMessage", Color.class);
+						if (clanColorConfigColor == null) {
+							int colorCode = transparent ? client.getVarpValue(3005) - 1 : client.getVarpValue(2997) - 1;
+							if (colorCode == 0) {
+								clanColorConfigColor = Color.BLACK;
+							} else if (colorCode == -1) {
+								clanColorConfigColor = transparent ? new Color(8323072) : new Color(8323072);
+							} else {
+								clanColorConfigColor = new Color(colorCode);
+							}
+						}
+						text = " <col=" + Integer.toHexString(clanColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
+						chat.setText(name + text);
+					} else if (text.startsWith("/") && client.getFriendsChatManager() != null) {
+						friendColorConfigColor = configManager.getConfiguration("textrecolor", transparent ? "transparentClanChatMessage" : "opaqueClanChatMessage", Color.class);
+						if (friendColorConfigColor == null) {
+							int colorCode = transparent ? client.getVarpValue(3004) - 1 : client.getVarpValue(2996) - 1;
+							if (colorCode == 0) {
+								friendColorConfigColor = Color.BLACK;
+							} else if (colorCode == -1) {
+								friendColorConfigColor = transparent ? new Color(0xEF5050) : new Color(8323072);
+							} else {
+								friendColorConfigColor = new Color(colorCode);
+							}
+						}
+						text = " <col=" + Integer.toHexString(friendColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
+						chat.setText(name + text);
+					} else {
+						if (selected == 0) {
+							unselectedColorConfigColor = transparent ? new Color(0x9090ff) : new Color(0x0000FF);
+							text = " <col=" + Integer.toHexString(unselectedColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
+							chat.setText(name + text);
+						} else if (selected == 1) { //clan
+							clanColorConfigColor = configManager.getConfiguration("textrecolor", transparent ? "transparentClanMessage" : "opaqueClanMessage", Color.class);
+							if (clanColorConfigColor == null) {
+								int colorCode = transparent ? client.getVarpValue(3005) - 1 : client.getVarpValue(2997) - 1;
+								if (colorCode == 0) {
+									clanColorConfigColor = Color.BLACK;
+								} else if (colorCode == -1) {
+									clanColorConfigColor = transparent ? new Color(8323072) : new Color(8323072);
+								} else {
+									clanColorConfigColor = new Color(colorCode);
+								}
+							}
+							text = " <col=" + Integer.toHexString(clanColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
+							chat.setText(name + text);
+						} else if (selected == 2 && client.getFriendsChatManager() != null) { //friend
+							friendColorConfigColor = configManager.getConfiguration("textrecolor", transparent ? "transparentClanChatMessage" : "opaqueClanChatMessage", Color.class);
+							if (friendColorConfigColor == null) {
+								int colorCode = transparent ? client.getVarpValue(3004) - 1 : client.getVarpValue(2996) - 1;
+								if (colorCode == 0) {
+									friendColorConfigColor = Color.BLACK;
+								} else if (colorCode == -1) {
+									friendColorConfigColor = transparent ? new Color(0xEF5050) : new Color(8323072);
+								} else {
+									friendColorConfigColor = new Color(colorCode);
+								}
+							}
+							text = " <col=" + Integer.toHexString(friendColorConfigColor.getRGB()).substring(2) + ">" + text + "*</col>";
+							chat.setText(name + text);
+						}
+					}
 			}
 		}
-
 	}
 
 	@Subscribe
@@ -88,11 +254,5 @@ public class InputRecolorPlugin extends Plugin
 		} else {
 			selected=0;
 		}
-	}
-
-	@Provides
-	InputRecolorConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(InputRecolorConfig.class);
 	}
 }
